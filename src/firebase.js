afd7b9f8fa927a5ba2a82668e0d0b3908a99f001
Sue419 <suzan419@gmail.com>
2023-06-06 12:20:03 -0500

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAnKOEfXjEtP0hUS-0ZRJ2L6q29OXsV7bo',
+  authDomain: 'labgram-53b0c.firebaseapp.com',
+  projectId: 'labgram-53b0c',
+  storageBucket: 'labgram-53b0c.appspot.com',
+  messagingSenderId: '240384744782',
+  appId: '1:240384744782:web:88c6c3bd80dece4d10521f',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
