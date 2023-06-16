47371033e774db38a016c15f16337e8afc9de1de
Nathaly Santos <nathalysantos0103@gmail.com>
2023-06-16 13:14:21 -0500

    cambios login

--- src/components/feed.js
+++ src/components/feed.js
@@ -2,29 +2,13 @@ import { QuerySnapshot } from 'firebase/firestore';
 import { crearPost, obtenerTodosLosPost } from '../lib';
 
 export const feed = (onNavigate) => {
-<<<<<<< HEAD
-// HTML
   const homeDiv = document.createElement('div');
-  homeDiv.textContent = 'Bienvenid@s a {LABGRAM]';
-  homeDiv.className = 'home-div';
-  const buttonHome = document.createElement('button');
-
-  buttonHome.classList = 'home-div__button';
-  buttonHome.textContent = 'Regresar al Home';
-
-  buttonHome.addEventListener('click', () => onNavigate('/'));
-
+  homeDiv.classList.add('containerFeed');
   homeDiv.innerHTML += `
-=======
-
-const homeDiv = document.createElement("div");
-homeDiv.classList.add("containerFeed");
-homeDiv.innerHTML += `
 <div class="form-container feed-container">
 <form class="textCenter">
 <h1>Bienvenid@s a {LABGRAM}</h1>
 <h2>Publicaciones</h2>
->>>>>>> 84eb943d787ea3719e0a0aeffca4c2d70a5de85e
 <div class="new-post__container">
   <textarea class="new-post__container__textarea" placeholder="Escribe aqui"></textarea>
   <button class="new-post__container__button">Publicar</button>
@@ -37,68 +21,38 @@ homeDiv.innerHTML += `
 </form>
 `;
 
-<<<<<<< HEAD
+  const buttonLogin = document.createElement('button');
+  buttonLogin.classList = 'home-div__button';
+  buttonLogin.textContent = 'Regresar al Login';
+  buttonLogin.addEventListener('click', () => onNavigate('/login'));
+
   homeDiv.querySelector('.new-post__container__button').addEventListener(
     'click',
     () => {
       const contenidoDelTextarea = homeDiv.querySelector('.new-post__container__textarea');
-      crearPost(contenidoDelTextarea.value).then(() => {
-        contenidoDelTextarea.value = '';
-        homeDiv.querySelector('.publicaciones').innerHTML = '';
-        obtenerTodosLosPost().then((QuerySnapshot) => {
-          QuerySnapshot.forEach((doc) => {
-            homeDiv.querySelector('.publicaciones').innerHTML += `
-              <div class="posts__post">
-                <p>${doc.data().contenido}</p>
-              </div>
-              `;
-          });
-        });
-      });
-      // console.log(contenidoDelTextarea.value);
+      console.log(contenidoDelTextarea.value);
+      //         crearPost(contenidoDelTextarea.value).then(() => {
+      //           contenidoDelTextarea.value = "";
+      //           alert('Publicación subida');
+      //         })
+      //         .catch((error) => {
+      //           alert(error);
+      //         });
     },
   );
-  // <h3>${doc.data().usuario}</h3>va en fila43
-  // <div class="usuario__user"><h3>Jo</h3></div> va en fila 25
+  //   homeDiv.querySelector('.publicaciones').innerHTML = '';
+  //   obtenerTodosLosPost().then((QuerySnapshot) => {
+  //     QuerySnapshot.forEach((doc) => {
+  //       homeDiv.querySelector('.publicaciones').innerHTML += `
+  //       <div class="posts__post">
+  //         <p>${doc.data().contenido}</p>
+  //       </div>
+  //       `;
+  //     });
+  //   });
+  // });
+  // console.log(contenidoDelTextarea.value);
 
-  homeDiv.appendChild(buttonHome);
+  homeDiv.appendChild(buttonLogin);
   return homeDiv;
 };
-=======
-const buttonLogin = document.createElement('button');
-buttonLogin.classList = 'home-div__button';
-buttonLogin.textContent = 'Regresar al Login';
-buttonLogin.addEventListener('click', () => onNavigate('/login'));
-
-homeDiv.querySelector('.new-post__container__button').addEventListener(
-    'click', 
-    () => {
-        const contenidoDelTextarea = homeDiv.querySelector('.new-post__container__textarea');
-        console.log(contenidoDelTextarea.value);
-//         crearPost(contenidoDelTextarea.value).then(() => {
-//           contenidoDelTextarea.value = "";
-//           alert('Publicación subida');
-//         })
-//         .catch((error) => {
-//           alert(error);
-//         });
-    }
-);
-        //   homeDiv.querySelector('.publicaciones').innerHTML = '';
-        //   obtenerTodosLosPost().then((QuerySnapshot) => {
-        //     QuerySnapshot.forEach((doc) => {
-        //       homeDiv.querySelector('.publicaciones').innerHTML += `
-        //       <div class="posts__post">
-        //         <p>${doc.data().contenido}</p>
-        //       </div>
-        //       `;
-        //     });
-        //   });
-        // });
-        // console.log(contenidoDelTextarea.value);
-
-
-homeDiv.appendChild(buttonLogin);
-return homeDiv;
-}
->>>>>>> 84eb943d787ea3719e0a0aeffca4c2d70a5de85e
--- src/lib/index.js
+++ src/lib/index.js
@@ -8,56 +8,31 @@ import {
 import { addDoc, collection, getDocs } from 'firebase/firestore';
 import { auth, db } from '../firebase';
 
-export const crearUsuarioYContraseña = (email, password) =>
-  createUserWithEmailAndPassword(auth, email, password); // agregue return para la promesa de console log de login fila 25
+export const crearUsuarioYContraseña = (email, password) => createUserWithEmailAndPassword(auth, email, password); // agregue return para la promesa de console log de login fila 25
 
-<<<<<<< HEAD
-export const loginUsuarioYContraseña = (email, password) =>
-signInWithEmailAndPassword(auth, email, password); // agregue return para la promesa de console log de login fila 25
+export const loginUsuarioYContraseña = (email, password) => signInWithEmailAndPassword(auth, email, password); // agregue return para la promesa de console log de login fila 25
 
-export const crearPost = (texto) =>
-  addDoc(collection(db, 'publicaciones'), {
-=======
-export const crearPost = async (texto) => {
-  await addDoc(collection(db, 'publicaciones'), {
->>>>>>> 84eb943d787ea3719e0a0aeffca4c2d70a5de85e
-    contenido: texto,
-    // usuario: user,
-  });
+export const crearPost = (texto) => addDoc(collection(db, 'publicaciones'), {
+  contenido: texto,
+  // usuario: user,
+});
 
-export const obtenerTodosLosPost = (texto) =>
-  getDocs(collection(db, 'publicaciones'), {
-    contenido: texto,
-    // usuario: user,
-  });
+export const obtenerTodosLosPost = (texto) => getDocs(collection(db, 'publicaciones'), {
+  contenido: texto,
+  // usuario: user,
+});
 
-<<<<<<< HEAD
-export const obtenerNombreUsuario = () =>
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      // ...
-=======
-export const obtenerNombreUsuario = () => {
-  return onAuthStateChanged(auth, (user) => {
-    if (obtenerNombreUsuario) {
+export const obtenerNombreUsuario = () => onAuthStateChanged(auth, (user) => {
+  if (obtenerNombreUsuario) {
     //  User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User 
-      const uid = user.uid;
-      //...
->>>>>>> 84eb943d787ea3719e0a0aeffca4c2d70a5de85e
-    } else {
-      // user is signed out
-    }
-  });
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // user is signed out
+  }
+});
 
-<<<<<<< HEAD
-=======
-
-}
-
->>>>>>> 84eb943d787ea3719e0a0aeffca4c2d70a5de85e
 // borrar post
 
 // obtener post
